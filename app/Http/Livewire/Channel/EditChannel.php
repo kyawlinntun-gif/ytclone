7f130ad65ef03a3d169e1179440f56a80510fb23
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2022-03-19 10:56:24 +0630

    1. Setup Channel Edit Routing, 2. Build The Edit Form, 3. Configure Validation via Livewire, and 4. Lock down Channel Update to owner via Authorization Policy

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -2,19 +2,50 @@
 
 namespace App\Http\Livewire\Channel;
 
+use App\Models\Channel;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class EditChannel extends Component
 {
-    public $name = "Tun Tun";
+    use AuthorizesRequests;
+
+    public $channel;
+
+    protected function rules()
+    {
+        return [
+            'channel.name' => 'required|max:255|unique:channels,name,' . $this->channel->id,
+            'channel.slug' => 'required|max:255|unique:channels,name,' . $this->channel->id,
+            'channel.description' => 'nullable|max:1000'
+        ];
+    }
+
+    public function mount(Channel $channel)
+    {
+        $this->channel = $channel;
+    }
 
     public function render()
     {
         return view('livewire.channel.edit-channel');
     }
 
-    public function submit()
+    public function update()
     {
-        dd('Submit');
+        $this->authorize('update', $this->channel);
+
+        $this->validate();
+
+        $this->channel->update([
+            'name' => $this->channel->name,
+            'slug' => $this->channel->slug,
+            'description' => $this->channel->description
+        ]);
+
+        Session::flash('message', 'Channel updated');
+
+        return redirect()->route('channel.edit', ['channel' => $this->channel]);
     }
 }
--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,7 +1,37 @@
 <div>
-    <form wire:submit.prevent='submit'>
-        <input type="text" wire:model='name'>
-        <button type="submit">Submit</button>
+    <form wire:submit.prevent="update">
+
+        <div class="mb-3">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" wire:model="channel.name">
+            @error('channel.name')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="slug">Slug</label>
+            <input type="text" class="form-control" id="slug" wire:model="channel.slug">
+            @error('channel.slug')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="description">Description</label>
+            <textarea id="description" rows="5" class="form-control" wire:model="channel.description"></textarea>
+            @error('channel.description')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+
+        @if (Session::has('message'))
+            <div class="alert alert-success mt-3">{{ Session::get('message') }}</div>
+        @endif
+
     </form>
-    {{ $name }}
 </div>
